921fda4ae33bb1cdfe6e953f30b8815e225ff0c0
Atharva Vichare <avichare09@gmail.com>
2026-08-24 14:52:39 +0200

    Fix relative import in train_model

--- src/train_model.py
+++ src/train_model.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-from load_data import load_data
+from src.load_data import load_data
 from preprocess import preprocess
 
 # 1. Load Data & Preprocess
