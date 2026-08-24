c4719d3d1f069a89433bbd25bf030bd7693129c4
Atharva Vichare <avichare09@gmail.com>
2026-08-24 15:00:46 +0200

    Create raw data directory automatically

--- src/load_data.py
+++ src/load_data.py
@@ -1,9 +1,14 @@
-from sklearn.datasets import fetch_california_housing
+import os
 import pandas as pd
+from sklearn.datasets import fetch_california_housing
 
 def load_data():
     housing = fetch_california_housing(as_frame=True)
-    df = housing.frame  # pandas DataFrame including target column 'MedHouseVal'
+    df = housing.frame
+    
+    # Ensure the output directory exists
+    os.makedirs("data/raw", exist_ok=True)
+    
     df.to_csv("data/raw/california_housing.csv", index=False)
     return df
 
